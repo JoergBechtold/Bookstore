53efc244714f99544049682994dcc536824ad49e
Jörg Bechtold <joergbechtold@gmx.net>
2025-01-08 17:15:16 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,210 @@
+let books = [
+  {
+    name: 'Die Geheimnisse des Ozeans',
+    author: 'Clara Meer',
+    bookImg: 'img/Die_Geheimnisse_des_Ozeans.png',
+    likes: 1250,
+    liked: true,
+    price: 19.99,
+    publishedYear: 2018,
+    genre: 'Fantasy',
+    comments: [
+      {
+        name: 'Leser123',
+        comment: 'Ein faszinierendes Abenteuerbuch, das mich von der ersten Seite an gefesselt hat.',
+      },
+      {
+        name: 'Bookworm84',
+        comment: 'Eine romantische Geschichte, die mein Herz berührt und mich zum Nachdenken gebracht hat.',
+      },
+      {
+        name: 'FantasyFanatic',
+        comment: 'Eine spannende Fantasiewelt, die ich nur schwer aus der Hand legen konnte.',
+      },
+      {
+        name: 'SciFiGuru',
+        comment: 'Ein cleverer Science-Fiction-Roman mit interessanten Zeitreise-Konzepten und Charakteren.',
+      },
+      {
+        name: 'NovelLover',
+        comment: 'Ein Buch, das voller magischer Überraschungen steckt und mich begeistert hat.',
+      },
+    ],
+  },
+  {
+    name: 'Der vergessene Pfad',
+    author: 'Maximilian Schwarz',
+    bookImg: 'img/Der_vergessene_Pfad.png',
+    likes: 980,
+    liked: false,
+    price: 14.5,
+    publishedYear: 2021,
+    genre: 'Fantasy',
+    comments: [],
+  },
+  {
+    name: 'Die Farben des Himmels',
+    author: 'Laura Blau',
+    bookImg: 'img/Die_Farben_des_Himmels.png',
+    likes: 1520,
+    liked: true,
+    price: 22.95,
+    publishedYear: 2019,
+    genre: 'Romantik',
+    comments: [
+      {
+        name: 'LeserPeter',
+        comment: 'Die Handlung war fesselnd und die Charaktere unglaublich lebendig dargestellt.',
+      },
+      {
+        name: 'BookLover21',
+        comment: 'Ein romantisches Meisterwerk, das mich tief berührt und bewegt hat.',
+      },
+      {
+        name: 'FantasyNerd',
+        comment: 'Fantastische Welten und epische Abenteuer - genau mein Geschmack!',
+      },
+      {
+        name: 'SciFiEnthusiast',
+        comment: 'Die Zeitreise-Elemente waren genial und haben die Story spannend gemacht.',
+      },
+      {
+        name: 'ReadingAddict',
+        comment: 'Ein unvergessliches Buch, das mich auf eine magische Reise mitgenommen hat.',
+      },
+    ],
+  },
+  {
+    name: 'Das Rätsel der Zeit',
+    author: 'Alexander Weiss',
+    bookImg: 'img/Das_Rätsel_der_Zeit.jpg',
+    likes: 750,
+    liked: false,
+    price: 18.0,
+    publishedYear: 2020,
+    genre: 'Science-Fiction',
+    comments: [
+      {
+        name: 'BuchKenner',
+        comment: 'Ein spannendes Abenteuer, das mich von Anfang an mitgerissen hat.',
+      },
+      {
+        name: 'LeseWurm',
+        comment: 'Die Liebesgeschichte war herzergreifend und wunderschön geschrieben.',
+      },
+    ],
+  },
+  {
+    name: 'Der letzte Wächter',
+    author: 'Sabine Grün',
+    bookImg: 'img/Der_Letzte_Wächter.jpg',
+    likes: 1300,
+    liked: true,
+    price: 16.75,
+    publishedYear: 2017,
+    genre: 'Fantasy',
+    comments: [],
+  },
+  {
+    name: 'Im Schatten des Mondes',
+    author: 'Philipp Silber',
+    bookImg: 'img/Im_Schatten_des_Mondes.jpg',
+    likes: 890,
+    liked: false,
+    price: 12.3,
+    publishedYear: 2022,
+    genre: 'Science-Fiction',
+    comments: [
+      {
+        name: 'BücherLiebhaber',
+        comment: 'Eine magische Reise durch eine faszinierende Fantasiewelt, absolut fesselnd.',
+      },
+      {
+        name: 'Leseratte',
+        comment: 'Ein packender Science-Fiction-Roman, der mich zum Nachdenken gebracht hat.',
+      },
+    ],
+  },
+  {
+    name: 'Jenseits der Sterne',
+    author: 'Oliver Schwarz',
+    bookImg: 'img/Jenseits_der_Sterne.jpg',
+    likes: 1450,
+    liked: true,
+    price: 21.0,
+    publishedYear: 2015,
+    genre: 'Science-Fiction',
+    comments: [
+      {
+        name: 'Leser123',
+        comment: 'Ein fesselndes Abenteuer, das mich von Anfang bis Ende mitgerissen hat.',
+      },
+    ],
+  },
+  {
+    name: 'Das verborgene Königreich',
+    author: 'Elena Gold',
+    bookImg: 'img/Das_verborgene_Königreich.jpg',
+    likes: 920,
+    liked: false,
+    price: 17.5,
+    publishedYear: 2020,
+    genre: 'Fantasy',
+    comments: [
+      {
+        name: 'Bookworm92',
+        comment: 'Ein faszinierendes Buch, das mich von der ersten Seite an gefesselt hat.',
+      },
+    ],
+  },
+  {
+    name: 'Liebe in Zeiten des Krieges',
+    author: 'Emilia Rot',
+    bookImg: 'img/Liebe_in_Zeiten_des_Krieges.jpg',
+    likes: 1800,
+    liked: true,
+    price: 19.99,
+    publishedYear: 2016,
+    genre: 'Romantik',
+    comments: [
+      {
+        name: 'Bibliophile23',
+        comment: 'Die Fantasiewelt war so lebendig, ich konnte das Buch kaum aus der Hand legen.',
+      },
+      {
+        name: 'StorySeeker',
+        comment: 'Eine unglaublich berührende Liebesgeschichte, die mich tief bewegt hat.',
+      },
+      {
+        name: 'SciFiExplorer',
+        comment: 'Spannende Zukunftsvisionen und interessante Charaktere machten diesen Roman einzigartig.',
+      },
+    ],
+  },
+];
+
+function init() {
+  let blabla = document.getElementById('blabla');
+  blabla.innerHTML = '';
+
+  for (let i = 0; i < books.length; i++) {
+    // let bookName = books[i].name;
+
+    blabla.innerHTML += /*html*/ `
+        <!-- <p>${books[i].name}</p>
+        <img src="${books[i].bookImg}" alt="Bild vom Buch ${books[i].name}"> -->
+  
+        <div onclick="console(${i})" class="card" style="width: 18rem;">
+         <img src="${books[i].bookImg}" class="card-img-top" alt="Bild vom Buch ${books[i].name}">
+         <div class="card-body">
+          <p class="card-text">${books[i].name}</p>
+         </div>
+        </div>
+  
+        `;
+  }
+}
+
+function console(i) {
+  alert('das ist das Buch "' + books[i].name + '"');
+}
