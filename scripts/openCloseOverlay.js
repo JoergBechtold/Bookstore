b2662caf12a9f2b0bb5804b8fae7fcc9576dd299
Jörg Bechtold <joergbechtold@gmx.net>
2025-01-13 12:14:50 +0100

    corrected openFullScreenOverlay function

--- scripts/openCloseOverlay.js
+++ scripts/openCloseOverlay.js
@@ -1,6 +1,9 @@
-function openFullScreenOverlay() {
+function openFullScreenOverlay(i) {
   document.body.classList.add('no-scroll');
   document.getElementById('fullscreen_overlay').style = '';
+  document.getElementById('headline_bookdetails').innerHTML = books[i].name;
+  document.getElementById('img_bookdetails').alt = `Bild vom Buch ${books[i].name}`;
+  document.getElementById('img_bookdetails').src = books[i].bookImg;
 }
 
 function closeFullscreenOverlay() {
